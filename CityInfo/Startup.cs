053b65ef33820936a1d6b874f76a3d973309764d
irada <mageramovai@gmail.com>
2020-04-20 14:23:54 +0300

    db seeded

--- CityInfo/Startup.cs
+++ CityInfo/Startup.cs
@@ -8,12 +8,18 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CityInfo
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -24,7 +30,7 @@ namespace CityInfo
 #else
             services.AddTransient<IMailService, CloudMailService>();
 #endif
-            string connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=CifyInfoDB;Trusted_Connection=True;";
+            string connectionString = _configuration["connectionStrings:cityInfoDBConnectionString"];
             services.AddDbContext<CityInfoContext>(o =>
             {
                 o.UseSqlServer(connectionString);
